3452907a468391d40db5fee730cd640e8aa7cf7e
Yohannes Dejene <yohannesdejene23@gmail.com>
2023-03-31 21:08:15 +0300

    routing

--- Client/src/Layouts/NavBar/NavBar.jsx
+++ Client/src/Layouts/NavBar/NavBar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./NavBar.css";
+import { NavLink } from "react-router-dom";
+
 function NavBar() {
   return (
     <div>
@@ -8,8 +10,32 @@ function NavBar() {
           <img src="air.jpg" alt="images_place" />
         </div>
         <div className="navright">
-          <h3 className="signup">Signup</h3>
-          <h3 className="signin">Log in</h3>
+          <NavLink
+            to="/about"
+            style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
+          >
+            About
+          </NavLink>
+          <NavLink
+            to="/contactus"
+            style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
+          >
+            Contact Us
+          </NavLink>
+
+          <NavLink
+            to="/register"
+            style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
+          >
+            Signup
+          </NavLink>
+
+          <NavLink
+            to="/login"
+            style={({ isActive }) => ({ color: isActive ? "red" : "black" })}
+          >
+            Login
+          </NavLink>
         </div>
       </div>
       <hr />
