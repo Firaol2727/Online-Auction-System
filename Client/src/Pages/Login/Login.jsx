6887e27b5f4a8d1e0f9226f6282bfb2ca4089223
Firaol Getachew <fraolgetachew2727@gmail.com>
2023-04-06 09:54:10 +0300

    footer

--- Client/src/Pages/Login/Login.jsx
+++ Client/src/Pages/Login/Login.jsx
@@ -1,6 +1,6 @@
-// import LoginForm from ".../Components/LoginForm";
-import {NavBar} from ".../Layouts/NavBar/NavBar";
-import {Footer} from "../../Layouts/Footer/Footer";
+import LoginForm from "../../Components/LoginForm/LoginForm";
+import NavBar from "../../Layouts/NavBar/NavBar";
+import Footer from "../../Layouts/Footer/Footer";
 import "./Login.css";
 
 export default function Login() {
