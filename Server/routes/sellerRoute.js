64237831c3d635e6cc24a4bc45476f41802c66c7
firaol2727 <fraolgetachew2727@gmail.com>
2023-06-03 16:13:03 +0300

    Deleted cors

--- Server/routes/sellerRoute.js
+++ Server/routes/sellerRoute.js
@@ -70,10 +70,6 @@ router.use(
   })
 );
 
-router.use(cors({
-    origin: ['http://localhost:7494','http://127.0.0.1:3000','http://127.0.0.1:5173'],
-    credentials:true,
-}));
 const authorizeSeller=async(req,res,next)=>{
     console.log(req.body);
     let {username,password}=req.body;
